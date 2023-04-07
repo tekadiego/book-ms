825bafd57ffacd06ca21b58320e5b4819edd71f7
diego teka <tekadiego23@gmail.com>
2023-04-07 10:03:12 +0800

    adding action to build and push to ecr

--- launch-app.sh
+++ launch-app.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "The student-app api is starting ..."
-#exec java -jar -Xms1g -Xmx2g -Dspring.profiles.active=k8s "student-app.jar"
+#exec java -jar -Xms1g -Xmx2g -Dspring.profiles.active=k8s "book-ms.jar"
 exec java -jar -Xms1g -Xmx2g "book-ms.jar"
